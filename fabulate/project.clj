8ae40aef03508ad26ec3c607232c3e05f3820b49
Martin Hellspong <martin.hellspong@factor10.com>
2013-05-31 13:26:42 +0200

    Point out main namespace to lein

--- fabulate/project.clj
+++ fabulate/project.clj
@@ -1,6 +1,7 @@
 (defproject fabulate "0.1.0-SNAPSHOT"
   :description "Fabulate generates data from your specifications"
   :url "http://github.com/marhel/fabulate"
+  :main fabulate.main
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
