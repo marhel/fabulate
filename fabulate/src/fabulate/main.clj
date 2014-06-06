583b3a727f742872223ae9c04548e37a609b76d7
Martin Hellspong <martin.hellspong@factor10.com>
2014-06-06 16:34:19 +0200

    Allow single field output
    By adding a third parameter naming the field to output

--- fabulate/src/fabulate/main.clj
+++ fabulate/src/fabulate/main.clj
@@ -5,9 +5,12 @@
 (defn -main [& args]
   (let [file (first args)
         n (read-string (or (second args) "10"))
+        only (when (< 2 (count args)) (nth args 2))
         code (slurp file)
         fields (parsing/parse :fields code)]
       (println "Fabulate 0.0.2α - Martin Hellspong")
       (println "-- Code from" file "--\n\n" code "\n\n-- Fields --\n" fields "\n\n--" n "Results --")
       (dotimes [i n]
-        (prn (generate fields)))))
+        (let [fields (generate fields)
+              fn (if only (keyword only) identity)]
+            (prn (fn fields))))))
