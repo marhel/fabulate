20848e8c5cf6a24c710162dc750e21c304b535c2
Martin Hellspong <martin.hellspong@factor10.com>
2014-06-06 14:27:05 +0200

    Upgrade instaparse to 1.3.2

--- fabulate/project.clj
+++ fabulate/project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [instaparse "1.3.1"]
+                 [instaparse "1.3.2"]
                  [re-rand "0.1.0"]
                  [org.clojure/data.csv "0.1.2"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]
